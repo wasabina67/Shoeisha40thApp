d506e8e5f97acdfd761d83f179a8e85687c80dfa
wasabina67 <wasabina67@gmail.com>
2025-07-19 14:25:44 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string[] art = {
+};
+
+art.ToList().ForEach(Console.WriteLine);
+Console.WriteLine(
+    "\n" +
+    "翔泳社40周年、おめでとうございます！\n" +
+    "これからも素晴らしい活動を応援しています！\n" +
+    "Happy 40th Anniversary, Shoeisha!"
+);
